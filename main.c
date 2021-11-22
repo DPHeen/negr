ed8408f50abd10328b3dffa4f2b2af96c5e2c744
DPHeen <70721885+DPHeen@users.noreply.github.com>
2021-11-22 17:21:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// sa gita
 #define n int
 #define nn main() {
-#define nnn while(1) {
-#define nnnn printf("negr");
-#define nnnnn }
-#define nnnnnn return 0;
+#define nnn char negr[4];
+#define nnnn negr[0] = 'n';
+#define nnnnn negr[1] = 'e';
+#define nnnnnn negr[2] = 'g';
+#define nnnnnnn negr[3] = 'r';
+#define nnnnnnnn negr[4] = '\0';
+#define nnnnnnnnn while(1) {
+#define nnnnnnnnnn printf(
+#define nnnnnnnnnnn "%c",
+#define nnnnnnnnnnnn negr[0]);
+#define nnnnnnnnnnnnn negr[1]);
+#define nnnnnnnnnnnnnn negr[2]);
+#define nnnnnnnnnnnnnnn negr[3]);
+#define nnnnnnnnnnnnnnnn "\n"
+#define nnnnnnnnnnnnnnnnn return 0;
+#define nnnnnnnnnnnnnnnnnn }
+#define nnnnnnnnnnnnnnnnnnn );
+#define nnnnnnnnnnnnnnnnnnnn " "
+
 
 n nn
 nnn
 nnnn
 nnnnn
 nnnnnn
-nnnnn
+nnnnnnn
+nnnnnnnn
+nnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn
