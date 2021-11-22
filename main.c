940563f5a9dfd70ec272631d08265cf1bf642dcb
DPHeen <70721885+DPHeen@users.noreply.github.com>
2021-11-22 18:18:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define n int
-#define nn main() {
-#define nnn char negr[4];
-#define nnnn negr[0] = 'n';
-#define nnnnn negr[1] = 'e';
-#define nnnnnn negr[2] = 'g';
-#define nnnnnnn negr[3] = 'r';
-#define nnnnnnnn negr[4] = '\0';
-#define nnnnnnnnn while(1) {
-#define nnnnnnnnnn printf(
+#define nn main
+#define nnn char
+#define nnnnn negr[1]
+#define nnnnnnnn negr[4]
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 1
+#define nnnnnnnnn while
+#define nnnnnnnnnn printf
 #define nnnnnnnnnnn "%c",
-#define nnnnnnnnnnnn negr[0]);
-#define nnnnnnnnnnnnn negr[1]);
-#define nnnnnnnnnnnnnn negr[2]);
-#define nnnnnnnnnnnnnnn negr[3]);
+#define nnnnnnnnnnnn negr[0]
+#define nnnnnnnnnnnnnn negr[2]
+#define nnnnnnnnnnnnnnn negr[3]
 #define nnnnnnnnnnnnnnnn "\n"
-#define nnnnnnnnnnnnnnnnn return 0;
-#define nnnnnnnnnnnnnnnnnn }
-#define nnnnnnnnnnnnnnnnnnn );
-#define nnnnnnnnnnnnnnnnnnnn " "
+#define nnnnnnnnnnnnnnnnn return
+#define nnnnnnnnnnnnnnnnnn {
+#define nnnnnnnnnnnnnnnnnnn }
+#define nnnnnnnnnnnnnnnnnnnn (
+#define nnnnnnnnnnnnnnnnnnnnn )
+#define nnnnnnnnnnnnnnnnnnnnnnn " "
+#define nnnnnnnnnnnnnnnnnnnnnnnn ;
+#define nnnnnnnnnnnnnnnnnnnnnnnnn =
+#define nnnnnnnnnnnnnnnnnnnnnnnnnn '
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnn 'n'
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnn 'e'
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnn 'g'
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 'r'
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn '\0'
+#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 0
 
 
-n nn
-nnn
-nnnn
-nnnnn
-nnnnnn
-nnnnnnn
-nnnnnnnn
-nnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn
+n nn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn
+nnn nnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnn  nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn 
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnnnnnnnnn
+nnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnn
