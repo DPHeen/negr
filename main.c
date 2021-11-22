e34f8b63f270af562a1a2b4acc5becd3d8eeca00
DPHeen <70721885+DPHeen@users.noreply.github.com>
2021-11-22 15:57:13 +0100

    Add files via upload
    i made the .out file for the true chads running C builds from terminal
    
    .оут фајл је за крштене чедомире који покрећу ц програме из терминала као прави православци

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n int
+#define nn main() {
+#define nnn while(1) {
+#define nnnn printf("negr");
+#define nnnnn }
+#define nnnnnn return 0;
+
+n nn
+nnn
+nnnn
+nnnnn
+nnnnnn
+nnnnn
