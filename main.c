b8d980e43c275a6191ecc3e4b66bd057a832c0ef
DPHeen <70721885+DPHeen@users.noreply.github.com>
2021-12-20 23:16:27 +0100

    Segmentation fault (core dumped)
    Process returned 139 (0x8B) 
    execution time 0.155s
    
    REALLY sorry

--- main.c
+++ main.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define n int
-#define nn main
-#define nnn char
-#define nnnnn negr[1]
-#define nnnnnnnn negr[4]
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 1
-#define nnnnnnnnn while
-#define nnnnnnnnnn printf
-#define nnnnnnnnnnn "%c",
-#define nnnnnnnnnnnn negr[0]
-#define nnnnnnnnnnnnnn negr[2]
-#define nnnnnnnnnnnnnnn negr[3]
-#define nnnnnnnnnnnnnnnn "\n"
-#define nnnnnnnnnnnnnnnnn return
-#define nnnnnnnnnnnnnnnnnn {
-#define nnnnnnnnnnnnnnnnnnn }
-#define nnnnnnnnnnnnnnnnnnnn (
-#define nnnnnnnnnnnnnnnnnnnnn )
-#define nnnnnnnnnnnnnnnnnnnnnnn " "
-#define nnnnnnnnnnnnnnnnnnnnnnnn ;
-#define nnnnnnnnnnnnnnnnnnnnnnnnn =
-#define nnnnnnnnnnnnnnnnnnnnnnnnnn '
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnn 'n'
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnn 'e'
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnn 'g'
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 'r'
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn '\0'
-#define nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn 0
 
+#define b int
+#define bb main
+#define bbb (
+#define bbbb )
+#define bbbbb {
+#define bbbbbb }
+#define bbbbbbb printf
+#define bbbbbbbb while
+#define bbbbbbbbb 1
+#define bbbbbbbbbb "
+#define bbbbbbbbbbb 's'
+#define bbbbbbbbbbbb 'h'
+#define bbbbbbbbbbbbb 'a'
+#define bbbbbbbbbbbbbb 'l'
+#define bbbbbbbbbbbbbbb 'o'
+#define bbbbbbbbbbbbbbbb 'm'
+#define bbbbbbbbbbbbbbbbb ' '
+#define bbbbbbbbbbbbbbbbbb 'v'
+#define bbbbbbbbbbbbbbbbbbb 'e'
+#define bbbbbbbbbbbbbbbbbbbb 'i'
+#define bbbbbbbbbbbbbbbbbbbbb 't'
+#define bbbbbbbbbbbbbbbbbbbbbb 'r'
+#define bbbbbbbbbbbbbbbbbbbbbbb ;
+#define bbbbbbbbbbbbbbbbbbbbbbbb ,
 
-n nn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn
-nnn nnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnn  nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnn 
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnn nnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnn nnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnnnnnnnnn
-nnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnnnnnnn nnnnnnnnnnnnnnnnnnn
+b bb bbb bbbb  bbbbb bbbbbbbb bbb bbbbbbbbb bbbb bbbbb
+bbbbbbb bbb bbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbb
+bbbb bbbbbbbbbbbbbbbbbbbbbbb bbbbbb bbbbbb
